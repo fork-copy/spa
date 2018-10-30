b633d6e43a54bf78b189e0e087cfd9f18a2d81d8
nyi <nyi@kurashicom.jp>
2018-10-30 13:12:00 +0900

    Update the vocabulary seeder

--- database/seeds/VocabulariesTableSeeder.php
+++ database/seeds/VocabulariesTableSeeder.php
@@ -24,27 +24,27 @@ class VocabulariesTableSeeder extends Seeder
                 'meaning_in_burmese' => 'ေက်နပ္ျခင္း။ ေက်နပ္မႈ။ အားရျခင္း။',
                 'created_at' => $now,
                 'updated_at' => $now
-            ],
-            [
-                'user_id' => 1,
-                'furigana' => 'ばっちりです！',
-                'hiragana' => 'ばっちりです！',
-                'romaji' => 'Batchiridesu!',
-                'meaning_in_english' => 'It is perfect!',
-                'meaning_in_burmese' => 'အပိုုအလိုုမရိွျပည့္စံုုေနတာပဲ။',
-                'created_at' => $now,
-                'updated_at' => $now
-            ],
-            [
-                'user_id' => 1,
-                'furigana' => '今日やること',
-                'hiragana' => 'きょやること',
-                'romaji' => 'Kyō yaru koto',
-                'meaning_in_english' => 'What i will do today',
-                'meaning_in_burmese' => 'ဒီေန႔လုုပ္မယ့္အရာေတြ',
-                'created_at' => $now,
-                'updated_at' => $now
             ]
         );
+        DB::table('vocabularies')->insert([
+            'user_id' => 1,
+            'furigana' => 'ばっちりです！',
+            'hiragana' => 'ばっちりです！',
+            'romaji' => 'Batchiridesu!',
+            'meaning_in_english' => 'It is perfect!',
+            'meaning_in_burmese' => 'အပိုုအလိုုမရိွျပည့္စံုုေနတာပဲ။',
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
+        DB::table('vocabularies')->insert([
+            'user_id' => 1,
+            'furigana' => '今日やること',
+            'hiragana' => 'きょやること',
+            'romaji' => 'Kyō yaru koto',
+            'meaning_in_english' => 'What i will do today',
+            'meaning_in_burmese' => 'ဒီေန႔လုုပ္မယ့္အရာေတြ',
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
     }
 }
